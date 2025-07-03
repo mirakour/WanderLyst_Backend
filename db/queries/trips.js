c5956c272d1c943196dfe48974df2ff05937a1fd
Sara Yared <sarayared@gmail.com>
2025-07-02 23:19:14 -0400

    test......

--- db/queries/trips.js
+++ db/queries/trips.js
@@ -1,5 +1,6 @@
 import db from "../client";
 
+
 export async function createTrip(title, description, start_date, end_date) {
   const result = await db.query(
     "INSERT INTO trip (title, description, start_date, end_date) VALUES ($1, $2, $3, $4) RETURNING *;",
