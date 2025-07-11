72201c6393748f3167cb9c63af897ed1d674be54
dylght <sarayared@gmail.com>
2025-07-10 21:34:13 -0400

    added formated trips.js queries and added user_id to trip_member schema

--- db/queries/trips.js
+++ db/queries/trips.js
@@ -1,30 +1,18 @@
 import db from "../client.js";
 
-// export async function createTrip({title, description, start_date, end_date, created_by,
-// }) const result = await db.query(
-//     `INSERT INTO trip (title, description, start_date, end_date, created_by) VALUES ($1, $2, $3, $4, $5) RETURNING *;`,
-//     [title, description, start_date, end_date, created_by]
-//   );
-//   console.log(result);
-//   return result;
-// }
-
-export async function createTrip(title, description, start_date, end_date, created_by,
-) {
+export async function createTrip({title, description, start_date, end_date, created_by,
+}) {
   const sql = `INSERT INTO trip (title, description, start_date, end_date, created_by) VALUES ($1, $2, $3, $4, $5) RETURNING *;`;
   const { rows: trips } = await db.query(sql, [title, description, start_date, end_date, created_by]);
   return trips;
 }
-
-
-
-
+//get all trips with logged in user
 export async function getTrip(id) {
   const sql = `SELECT * FROM trip where created_by = $1;`;
   const { rows: trips } = await db.query(sql, [id]);
   return trips;
 }
-
+//get a trip of a certain id
 export async function getTripId(id) {
   const sql = `SELECT * from trip WHERE id = $1;`;
   const { rows: trips } = await db.query(sql, [id]);
